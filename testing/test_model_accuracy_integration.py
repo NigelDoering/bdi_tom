bc079ead4baeaf4b158f1cf148acd8d705d21f32
NigelDoering <nigelfd12@gmail.com>
2025-12-14 18:13:32 -0800

    Fix imports

--- testing/test_model_accuracy_integration.py
+++ testing/test_model_accuracy_integration.py
@@ -20,7 +20,7 @@ import networkx as nx
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from archive.utils import compute_accuracy, load_checkpoint, get_device
-from archive.data_loader import load_simulation_data, split_data, create_dataloaders
+from models.training.data_loader import load_simulation_data, split_data, create_dataloaders
 from models.transformer_predictor import GoalPredictionModel
 from models.encoders.trajectory_encoder import TrajectoryDataPreparator
 from models.encoders.map_encoder import GraphDataPreparator
